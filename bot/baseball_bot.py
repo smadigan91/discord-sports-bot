082afb5628e37209e369681b659d80dc3a172633
Sean <pipgargery@gmail.com>
2018-05-16 17:55:21 -0400

    get pretty game log output working, add functionality for last x games, better error handling

--- bot/baseball_bot.py
+++ bot/baseball_bot.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import datetime as dt
 import datetime
 from difflib import SequenceMatcher
 import json
@@ -28,19 +29,38 @@ class SportsClient(discord.Client):
     
     @asyncio.coroutine
     def on_message(self, message):
-        if(message.content.startswith('/blurb')):
-            if(message.channel.name == "sportsbot-testing"):
+        if(message.channel.name == "sportsbot-testing"):
+            if(message.content.startswith('/blurb')):
+                msg = message.content.split()[1:]
                 try:
-                    name = message.content.split()[1:]
-                    first = name[0]
-                    last = " ".join(name[1:])  # hopefully handles the Jrs
+                    first = msg[0]
+                    last = " ".join(msg[1:])  # hopefully handles the Jrs
                     if not first or not last:
                         raise ValueError('A first and last name must be provided')
                     blurb = fetch_blurb(first, last)
                     embedded_blurb = discord.Embed(title=" ".join([first, last]).title(), description=blurb)
                     yield from self.send_message(message.channel, embed=embedded_blurb)
-                except NoResultsError as ex:
-                    yield from self.send_message(message.channel, content=ex.message)
+                except Exception as ex:
+                    yield from self.send_message(message.channel, content=str(ex))
+            if(message.content.startswith('/last')):
+                msg = message.content.split()[1:]
+                try:
+                    days = int(msg[0])
+                    if not days:
+                        raise ValueError('A number of last days must be provided')
+                    if(len(msg) < 2):
+                        raise ValueError('Must provide both a number of days and a name')
+                    embedded_stats = fetch_player_stats(" ".join(msg[1:]), last_days=days)
+                    yield from self.send_message(message.channel, embed=embedded_stats)
+                except Exception as ex:
+                    yield from self.send_message(message.channel, content=str(ex))
+            if(message.content.startswith('/log')):
+                try:
+                    search = " ".join(message.content.split()[1:])
+                    embedded_stats = fetch_player_stats(search)
+                    yield from self.send_message(message.channel, embed=embedded_stats)
+                except Exception as ex:
+                    yield from self.send_message(message.channel, content=str(ex))
 
 
 def fetch_blurb(first, last, player_url=None):
@@ -62,7 +82,6 @@ def fetch_blurb(first, last, player_url=None):
         # sort names by similarity to search criteria
         sorted_names = sorted(name_map, key=name_map.get, reverse=True)
         return fetch_blurb(first, last, 'http://www.rotoworld.com' + sorted_names[0].get('href'))  # this should work?
-#             print(result.find('a').get('href'))
     else:
         news = soup.findChildren('div', class_='playernews')
         if news:
@@ -80,85 +99,148 @@ def fetch_blurb(first, last, player_url=None):
 
 # just better to do a search and find the best matching result
 # maybe add specific date functionality eventually?
-def fetch_player_stats(search, stat_type=None, player_url=None):
+def fetch_player_stats(search, stat_type=None, player_url=None, date=None, last_days=None, date_range=None):
     response = get(player_url if player_url else search_url.format(search=urllib.parse.quote(search)))
     soup = BeautifulSoup(response.text, 'html.parser')
     log_holder = soup.find('span', text="Game Logs")
     if log_holder:
-        name = soup.find('h1', attrs={'itemprop':'name'}).text
-        batting = None if (stat_type and stat_type != "b") else log_holder.find_next('div').find('p', class_='listhead', text='Batting')
-        pitching = None if (stat_type and stat_type != "p") else log_holder.find_next('div').find('p', class_='listhead', text='Pitching')
+        name_node = soup.find('h1', attrs={'itemprop':'name'})
+        name = name_node.text
+        is_pitcher = "Pitcher" in name_node.find_next('p').text  # next element after name contains positions
+        batting = None if ((stat_type and stat_type != "b") or is_pitcher) else log_holder.find_next('div').find('p', class_='listhead', text='Batting')
+        pitching = None if ((stat_type and stat_type != "p") or not is_pitcher) else log_holder.find_next('div').find('p', class_='listhead', text='Pitching')
         if batting or pitching:
-            batting_list = [] if not batting else batting.find_next('ul').findChildren(lambda tag:tag.name == 'a' and tag.text != 'Postseason')
-            pitching_list = [] if not pitching else pitching.find_next('ul').findChildren(lambda tag:tag.name == 'a' and tag.text != 'Postseason')
-            if len(batting_list) > len(pitching_list):
+            batting_list = None if not batting else batting.find_next('ul').findChildren(lambda tag:tag.name == 'a' and tag.text != 'Postseason')
+            pitching_list = None if not pitching else pitching.find_next('ul').findChildren(lambda tag:tag.name == 'a' and tag.text != 'Postseason')
+            if batting:
                 href = batting_list.pop().get('href')
-                return print_most_recent_game(name, bbref_url + href, 'b',)
+                return parse_player_stats(name, bbref_url + href, 'b', date, last_days, date_range)
                 # grab batting logs
-            elif len(pitching_list) > len(batting_list):
+            elif pitching:
                 href = pitching_list.pop().get('href')
-                return print_most_recent_game(name, bbref_url + href, 'p')
-                # grab pitching logs
-            elif len(batting_list) == len(pitching_list):
-                # ask to disambiguate
-                raise ValueError("Equal number of years of recorded batting and pitching logs. Try disambiguating with `/logb` for batting and `/logp` for pitching")
+                return parse_player_stats(name, bbref_url + href, 'p', date, last_days, date_range)
             else:
                 raise ValueError("Not sure what happened, landed in a player page without accessible game logs [%s]" % search)
         else:
             if stat_type == "b":
-                raise NoResultsError("No batting stats for %s" % search)
+                raise NoResultsError("No batting stats for %s" % name)
             elif stat_type == "p":
-                raise NoResultsError("No pitching stats for %s" % search)
+                raise NoResultsError("No pitching stats for %s" % name)
         
     elif soup.findChild('div', class_='search-results'):
         href = soup.find('div', attrs={"id":"players"}).find_next('div', class_='search-item-url').text
-        return fetch_player_stats(search, stat_type, bbref_url + href)
+        return fetch_player_stats(search, stat_type, bbref_url + href, date, last_days, date_range)
     else:
         raise NoResultsError("No results for %s" % search)
 
     
-def print_most_recent_game(name, gamelog_url, player_type):
+def parse_player_stats(name, gamelog_url, player_type, date=None, last_days=None, date_range=None):
     response = get(gamelog_url)
     soup = BeautifulSoup(response.text, 'html.parser')
     tag_id = 'batting_gamelogs' if player_type == 'b' else 'pitching_gamelogs'
-    table = soup.find('table', attrs={'id':tag_id}).find('tbody').find_all('tr')
-    most_recent = table.pop()
+    table = []
+    if last_days or date:
+        end_date = datetime.date.today()
+        start_date = end_date - datetime.timedelta(days=last_days)
+        date_range = (start_date, end_date)
+        # filters the list of game rows down to those matching the date range
+        table = soup.find('table', attrs={'id':tag_id}).find('tbody').findChildren(
+            lambda tag:tag.name == 'tr' and not tag.get('class') == 'thead' and tag.findChild(
+                lambda tag:tag.name == "td" and tag['data-stat'] == 'date_game'
+                and ((start_date <= dt.strptime(tag.get('csk').split(".")[0], "%Y-%m-%d").date() <= end_date) if not date 
+                     else (dt.strptime(tag.get('csk').split(".")[0], "%Y-%m-%d").date() == dt.strptime(date, "%Y-%m-%d").date()))))
+    else:
+        table = [soup.find('table', attrs={'id':tag_id}).find('tbody').find_all('tr').pop()]
+    if not table:
+        if date:
+            raise NoResultsError("No results for %s on %s" % (name, date))
+        else:
+            raise NoResultsError("No results for %s from %s to %s" % (name, start_date, end_date))
+        # check if today >= date >= today - end_date
+    # if last_days, do a lambda to include the last days worth of table rows:
+    # for each row that has a child with date_game data stat and the csk split value within the range
+#     most_recent = table.pop()
     cat_dict = {}
     # index the stats
+    for row in table:
+        rollup_game_row(row, player_type, cat_dict)
+    return format_player_stats(name, player_type, cat_dict, date_range)
+
+    
+def format_player_stats(name, player_type, stat_map, date_range):
+    title = ""
+    body = ""
+    if not date_range:
+        title = "**%s** on **%s** vs **%s**" % (name, stat_map['DATE'], stat_map['VS'])
+    else:
+        title = "**%s** from **%s** through **%s**" % (name, date_range[0], date_range[1])
+        
+    if date_range: body += display('GP', stat_map)
+    if player_type == 'p':
+        if not date_range: body += display('DEC', stat_map)
+        body += display('IP', stat_map)
+        body += display('H', stat_map)
+        body += display('R', stat_map)
+        body += display('ER', stat_map)
+        body += display('BB', stat_map)
+        body += display('SO', stat_map)
+        body += display('ERA', stat_map)
+        body += display('WHIP', stat_map)
+        body += display('pitches', stat_map, "#PITCH")
+    else:
+        body += display('AVG', stat_map)
+        body += display('R', stat_map)
+        body += display('2B', stat_map)
+        body += display('3B', stat_map)
+        body += display('HR', stat_map)
+        body += display('RBI', stat_map)
+        body += display('BB', stat_map)
+        body += display('SO', stat_map)
+    return discord.Embed(title=title, description=body)
+    # if just one day, title should be:
+    # "Stats for <Name> on <Date> vs <OPP>
+    # if multi days, title should be:
+    # "Stats for <Name> from <start> thru <end>
+
+
+def display(key, stats, override=None):
+    return "**" + (key if not override else override) + ':** ' + str(stats[key]) + "\n"
+
+    
+def rollup_game_row(row, player_type, stat_map):
+    stat_map['GP'] = stat_map.get('GP', 0) + 1
     if player_type == 'b':
-        for category in most_recent.find_all('td'):
+        for category in row.find_all('td'):
             stat = category.get('data-stat')
             if stat in batter_log_stats:
                 if stat == "date_game":
                     val = category.get('csk').split(".")[0]
-                    cat_dict["DATE"] = val
+                    stat_map['DATE'] = val
                 elif stat == "opp_ID":
                     val = category.findChild('a').text
-                    cat_dict["VS"] = val
-                else: cat_dict[stat] = category.text
-        HAB = cat_dict['H'] + "/" + cat_dict['AB']
-        AVG = ("%.3f" % round(int(cat_dict['H']) / int(cat_dict['AB']), 3)).lstrip('0')
-        cat_dict['AVG'] = AVG + (" (%s)" % HAB)
+                    stat_map['VS'] = val
+                else: stat_map[stat] = stat_map.get(stat, 0) + int(category.text)
+        HAB = str(stat_map['H']) + "/" + str(stat_map['AB'])
+        AVG = ".000" if stat_map['AB'] == '0' else ("%.3f" % round(int(stat_map['H']) / int(stat_map['AB']), 3)).lstrip('0')
+        stat_map['AVG'] = AVG + (" (%s)" % HAB)
     else:
-        for category in most_recent.find_all('td'):
+        for category in row.find_all('td'):
             stat = category.get('data-stat')
             if stat in pitcher_log_stats:
                 if stat == "date_game":
                     val = category.get('csk').split(".")[0]
-                    cat_dict["DATE"] = val
+                    stat_map["DATE"] = val
                 elif stat == "opp_ID":
                     val = category.findChild('a').text
-                    cat_dict["VS"] = val
+                    stat_map["VS"] = val
                 elif stat == "player_game_result":
                     val = "N/A" if not category.text else category.text
-                    cat_dict["DEC"] = val
-                else: cat_dict[stat] = category.text
-        ERA = str(round((9 * (int(cat_dict['ER']) / float(cat_dict['IP']))), 2))
-        WHIP = str(round(((int(cat_dict['BB']) + int(cat_dict['H']))) / float(cat_dict['IP']), 2))
-        cat_dict["ERA"] = ERA
-        cat_dict["WHIP"] = WHIP
-    print(name)
-    print(cat_dict)
+                    stat_map["DEC"] = val
+                else: stat_map[stat] = stat_map.get(stat, 0) + (int(category.text) if not stat == 'IP' else float(category.text))
+        ERA = "0.00" if stat_map['IP'] == '0' else '{0:.2f}'.format(round((9 * (int(stat_map['ER']) / float(stat_map['IP']))), 3))
+        WHIP = "0.00" if stat_map['IP'] == '0' else '{0:.2f}'.format(round(((int(stat_map['BB']) + int(stat_map['H']))) / float(stat_map['IP']), 3))
+        stat_map['ERA'] = ERA
+        stat_map['WHIP'] = WHIP
 
 
 # TODO return date, opponent, make top x configurable
@@ -227,11 +309,11 @@ class NoResultsError(Exception):
         self.message = message
 
 
-fetch_player_stats("Grandal", stat_type="b")
+# fetch_player_stats("Joe Kelly")
 # testing\
-# TOKEN = json.loads(open('../token.json','r').read())["APP_TOKEN"]
-# client = SportsClient()
-# client.run(TOKEN)
+TOKEN = json.loads(open('../token.json', 'r').read())["APP_TOKEN"]
+client = SportsClient()
+client.run(TOKEN)
 # fetch_blurb("JD", "Martinez")
 # best_pitchers()
 # print("\n")
