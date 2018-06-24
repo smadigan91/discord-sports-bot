2e1b7105b0ed3e6bffdbdefb706b92318bfa640d
Sean <pipgargery@gmail.com>
2018-06-24 09:38:08 -0400

    forgot a newline

--- bot/help_commands.py
+++ bot/help_commands.py
@@ -2,9 +2,9 @@ help_map = {
     "**/blurb [first_name: str]`*` [last_name: str]`*`**": "\nFetches the most recent blurb for a given player\n",
 
     "**/last [num_days: int]`*` [player_search: str]`*`**": "\nFetches the last [num_days] worth of stats for a given "
-                                                            "player",
+                                                            "player\n",
 
-    "**/log [player_search: str]`*`**": "Fetches the most recent gamelog for a given player\n",
+    "**/log [player_search: str]`*`**": "\nFetches the most recent gamelog for a given player\n",
 
     "**/season [year: int] [player_search: str]`*`**": "\nfetches current season's (unless otherwise specified) stats "
                                                        "for a given player\n",
