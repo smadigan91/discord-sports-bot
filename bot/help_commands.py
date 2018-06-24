e759edba38164a1aba4b1c29e243e46ef0fb6765
Sean <pipgargery@gmail.com>
2018-06-24 09:34:06 -0400

    trying different formatting for discord

--- bot/help_commands.py
+++ bot/help_commands.py
@@ -1,13 +1,19 @@
 help_map = {
-    "**/blurb `[first_name: str]` `[last_name: str]`**": "Fetches the most recent blurb for a given player",
-    "**/last `[num_days: int]` `[player_search: str]`**": "Fetches the last [num_days] worth of stats for a given "
-                                                          "player",
-    "**/log `[player_search: str]`**": "Fetches the most recent gamelog for a given player",
-    "**/season [year: int] `[player_search: str]`**": "fetches current season's (unless otherwise specified) stats for "
-                                                      "a given player",
-    "**/highlight `[player_search: str]` [index: int]**": "Fetches the most recent highlight (or highlight at index if "
-                                                     "specified) for a given player",
-    "**/highlight index `[player_search: str]`**": "Lists the last 10 highlights and their index number for a given player"
+    "**/blurb [first_name: str]`*` [last_name: str]`*`**": "\nFetches the most recent blurb for a given player\n",
+
+    "**/last [num_days: int]`*` [player_search: str]`*`**": "\nFetches the last [num_days] worth of stats for a given "
+                                                            "player",
+
+    "**/log [player_search: str]`*`**": "Fetches the most recent gamelog for a given player\n",
+
+    "**/season [year: int] [player_search: str]`*`**": "\nfetches current season's (unless otherwise specified) stats "
+                                                       "for a given player\n",
+
+    "**/highlight [player_search: str]`*` [index: int]**": "\nFetches the most recent highlight (or highlight at index "
+                                                           "if specified) for a given player\n",
+
+    "**/highlight index [player_search: str]`*`**": "\nLists the last 10 highlights and their index number for a given "
+                                                    "player\n"
 }  # commands to descriptions, * designating required
 
 
