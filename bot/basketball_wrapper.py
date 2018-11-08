1d8bbd0d9ac2f429b089bd25298d8a408d3c6bf9
Sean <pipgargery@gmail.com>
2018-11-07 22:41:14 -0500

    better conditional

--- bot/basketball_wrapper.py
+++ bot/basketball_wrapper.py
@@ -68,12 +68,12 @@ def get_live_log_map(search, url=None):
         try:
             name = soup.find('div', class_='main-headshot').find_next('h1').text
             qtr = soup.find('li', class_='game-clock')
-            end_qtr = 'Halftime' in qtr.text or 'End' in qtr.text
-            if 'CURRENT' in profile.find_next('h4').text or end_qtr:
+            if qtr:
                 log_map = {}
                 time_left = qtr.find_next('span').text
                 log_header = profile.find_next(lambda tag: tag.name == 'h4' and tag.text == 'GAME LOG')
                 stats = log_header.find_next('table', class_='tablehead').findChildren('td')
+                end_qtr = 'Halftime' in qtr.text or 'End' in qtr.text
                 log_map['date_game'] = qtr.text.rstrip() if end_qtr else '{}, {}'.format(qtr.text.replace('"', '').replace(time_left, ''), time_left)
                 log_map['opp_id'] = stats[1].find_next('a').find_next('a').text
                 log_map['mp'] = stats[3].text
